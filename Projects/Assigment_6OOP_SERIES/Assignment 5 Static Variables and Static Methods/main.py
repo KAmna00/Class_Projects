bb88d19885295d4460836dbcba573a9069473fc8
Amnaa <160736145+KAmna00@users.noreply.github.com>
2025-05-06 20:10:12 +0500

    Delete Projects/Assigment_6OOP_SERIES directory

--- Projects/Assigment_6OOP_SERIES/Assignment 5 Static Variables and Static Methods/main.py
+++
@@ -1,6 +0,0 @@
-class MathUtils:
-    @staticmethod
-    def add_numbers(a, b):
-        return a + b
-
-print(MathUtils.add_numbers(5, 3))
--- Projects/Assigment_6OOP_SERIES/Assignment 6 Constructors and Destructors/main.py
+++
@@ -1,9 +0,0 @@
-class Logger:
-    def __init__(self):
-        print("Logger initialized. Ready to log messages.")
-
-    def __del__(self):
-        print("Logger destroyed. Cleanup complete.")
-
-logger = Logger()
-del logger
